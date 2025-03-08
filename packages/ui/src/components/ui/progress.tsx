4c6f57d91fd5a79a1c3ef714fa4a899ea690a7f9
Dris Elamri <elamri@dris.llc>
2025-03-08 09:28:54 -0600

    changes

--- packages/ui/src/components/ui/progress.tsx
+++ packages/ui/src/components/ui/progress.tsx
@@ -1,28 +1,52 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+
+export type ProgressVariant = "default" | "secondary" | "destructive" | "success" | "blocked";
+
+const variantClassesMap = {
+  default: {
+    root: "bg-formscale/20",
+    indicator: "bg-formscale",
+  },
+  secondary: {
+    root: "bg-secondary/20",
+    indicator: "bg-secondary",
+  },
+  destructive: {
+    root: "bg-destructive/10",
+    indicator: "bg-destructive",
+  },
+  success: {
+    root: "bg-green-300/20",
+    indicator: "bg-green-300",
+  },
+  blocked: {
+    root: "bg-destructive/20",
+    indicator: "bg-destructive/80",
+  },
+} as const;
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+    variant?: ProgressVariant;
+  }
+>(({ className, value, variant = "default", ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
-      className
-    )}
+    className={cn("relative h-2 w-full overflow-hidden rounded-full", variantClassesMap[variant].root, className)}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className={cn("h-full w-full flex-1 transition-all", variantClassesMap[variant].indicator)}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
-))
-Progress.displayName = ProgressPrimitive.Root.displayName
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
 
-export { Progress }
+export { Progress };
